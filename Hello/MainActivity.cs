0f2da59835e15368c28df85842b3a4d8a45f873a
Мария Фомина < >
2016-03-17 17:44:55 +0500

    Удален экран настроек. Вместо него добавлен navigation drawer

--- Hello/MainActivity.cs
+++ Hello/MainActivity.cs
@@ -1,46 +1,89 @@
-﻿using System;
-using Android.App;
+﻿using Android.App;
 using Android.Content;
-using Android.Runtime;
+using Android.OS;
+using Android.Support.V4.Widget;
 using Android.Views;
 using Android.Widget;
-using Android.OS;
 using Java.IO;
 using Newtonsoft.Json;
+using System;
 
 namespace Hello
 {
-    [Activity(Label = "@string/NameApp", Theme = "@style/MyCustomTheme")]//назваие в Actionbar 
+    [Activity(Label = "@string/NameApp", Theme = "@style/MyCustomTheme")]
     public class MainActivity : Activity
     {
         Button _connectButton;
         Person per = new Person("Вольман", "Денис", "ООО «ФТ-СОФТ»", "+79226046146");
         string FILENAME = "contacts.txt";
         const int DIALOG_ID = 0; //статическое число, однозначно идентифицирующее диалоговое окно
+        
+        private DrawerLayout m_DrawerLayout;
+        private ListView m_RightDrawerList;
+        private static readonly string[] Section = new[]
+            {
+                "Item1", "Item2", "Item3"
+            };
+        //private string m_DrawerTitle;
+        //private string m_Title;
 
-        protected override void OnCreate(Bundle bundle) //метод создания самого layout
+
+        protected override void OnCreate(Bundle bundle) 
         {
             base.OnCreate(bundle);
             //RequestedOrientation = Android.Content.PM.ScreenOrientation.Portrait;
+
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-            
+           
+            //m_Title = m_DrawerTitle = Title;
+            m_DrawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            m_RightDrawerList = FindViewById<ListView>(Resource.Id.right_drawer);
+            m_RightDrawerList.Adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, Section);
+            //m_DrawerList.ItemClick += DrawerListOnItemClick;
 
             // Get our button from the layout resource,
             // and attach an event to it
             _connectButton = FindViewById<Button>(Resource.Id.myButton);
             _connectButton.Click += ConnectButton_Click;
 
-            Button exampleButton = FindViewById<Button>(Resource.Id.exampleButton);
-            exampleButton.Click += (o, e) => ShowDialog(0);
+            Button tempButton = FindViewById<Button>(Resource.Id.tempButton);
+            tempButton.Click += (o, e) => ShowDialog(0);
 
         }
+
+
+        //private void DrawerListOnItemClick(object sender, AdapterView.ItemClickEventArgs itemClickEventArgs)
+        //{
+        //    Android.Support.V4.App.Fragment fragment = null;
+        //    switch (itemClickEventArgs.Position)
+        //    {
+        //        case 0:
+        //            fragment = new BrowseFragment();
+        //            break;
+        //        case 1:
+        //            fragment = new FriendsFragment();
+        //            break;
+        //        case 2:
+        //            fragment = new ProfileFragment();
+        //            break;
+        //    }
+
+        //    SupportFragmentManager.BeginTransaction()
+        //        .Replace(Resource.Id.content_frame, fragment)
+        //        .Commit();
+
+        //    m_DrawerList.SetItemChecked(itemClickEventArgs.Position, true);
+        //    ActionBar.Title = m_Title = Sections[itemClickEventArgs.Position];
+        //    m_Drawer.CloseDrawer(m_DrawerList);
+        //}
+
 
         void ConnectButton_Click(object sender, EventArgs e)
         {
             if (_connectButton.Text != "@string/Hello")
             {
-                _connectButton.Text = string.Format("Здравствуйте, Иван!"); // взятие имени из браслета по id(в идеале)
+                _connectButton.Text = string.Format("Здравствуйте, Иван!"); // взятие имени из браслета по id
             }
         }
 
@@ -61,10 +104,18 @@ namespace Hello
                     StartActivity(contactsIntent);
                     break;
                 case Resource.Id.settings:
-                    contactsIntent = new Intent(this, typeof(SettingsActivity));
-                    StartActivity(contactsIntent);
-                    break;
+                    if (m_DrawerLayout.IsDrawerOpen(m_RightDrawerList))
+                    {
+                        //Right Drawer is already open, close it
+                        m_DrawerLayout.CloseDrawer(m_RightDrawerList);
+                    }
 
+                    else
+                    {
+                        //Right Drawer is closed, open it 
+                        m_DrawerLayout.OpenDrawer(m_RightDrawerList);                        
+                    }                   
+                    break;
             }
             return base.OnOptionsItemSelected(item);
         }
--- Hello/Resources/Resource.Designer.cs
+++ Hello/Resources/Resource.Designer.cs
@@ -1462,8 +1462,8 @@ namespace Hello
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f0b0059
-			public const int action0 = 2131427417;
+			// aapt resource value: 0x7f0b005c
+			public const int action0 = 2131427420;
 			
 			// aapt resource value: 0x7f0b0041
 			public const int action_bar = 2131427393;
@@ -1489,8 +1489,8 @@ namespace Hello
 			// aapt resource value: 0x7f0b0042
 			public const int action_context_bar = 2131427394;
 			
-			// aapt resource value: 0x7f0b005d
-			public const int action_divider = 2131427421;
+			// aapt resource value: 0x7f0b0060
+			public const int action_divider = 2131427424;
 			
 			// aapt resource value: 0x7f0b0002
 			public const int action_menu_divider = 2131427330;
@@ -1528,14 +1528,14 @@ namespace Hello
 			// aapt resource value: 0x7f0b0056
 			public const int buttonSms = 2131427414;
 			
-			// aapt resource value: 0x7f0b005a
-			public const int cancel_action = 2131427418;
+			// aapt resource value: 0x7f0b005d
+			public const int cancel_action = 2131427421;
 			
 			// aapt resource value: 0x7f0b0039
 			public const int checkbox = 2131427385;
 			
-			// aapt resource value: 0x7f0b0060
-			public const int chronometer = 2131427424;
+			// aapt resource value: 0x7f0b0063
+			public const int chronometer = 2131427427;
 			
 			// aapt resource value: 0x7f0b001c
 			public const int collapseActionView = 2131427356;
@@ -1545,6 +1545,9 @@ namespace Hello
 			
 			// aapt resource value: 0x7f0b0031
 			public const int contentPanel = 2131427377;
+			
+			// aapt resource value: 0x7f0b0058
+			public const int content_frame = 2131427416;
 			
 			// aapt resource value: 0x7f0b0037
 			public const int custom = 2131427383;
@@ -1561,17 +1564,17 @@ namespace Hello
 			// aapt resource value: 0x7f0b000c
 			public const int disableHome = 2131427340;
 			
+			// aapt resource value: 0x7f0b0057
+			public const int drawer_layout = 2131427415;
+			
 			// aapt resource value: 0x7f0b0043
 			public const int edit_query = 2131427395;
 			
 			// aapt resource value: 0x7f0b0019
 			public const int end = 2131427353;
 			
-			// aapt resource value: 0x7f0b0065
-			public const int end_padder = 2131427429;
-			
-			// aapt resource value: 0x7f0b0058
-			public const int exampleButton = 2131427416;
+			// aapt resource value: 0x7f0b0068
+			public const int end_padder = 2131427432;
 			
 			// aapt resource value: 0x7f0b0025
 			public const int expand_activities_button = 2131427365;
@@ -1594,14 +1597,14 @@ namespace Hello
 			// aapt resource value: 0x7f0b0026
 			public const int image = 2131427366;
 			
-			// aapt resource value: 0x7f0b0064
-			public const int info = 2131427428;
+			// aapt resource value: 0x7f0b0067
+			public const int info = 2131427431;
 			
-			// aapt resource value: 0x7f0b005e
-			public const int line1 = 2131427422;
+			// aapt resource value: 0x7f0b0061
+			public const int line1 = 2131427425;
 			
-			// aapt resource value: 0x7f0b0062
-			public const int line3 = 2131427426;
+			// aapt resource value: 0x7f0b0065
+			public const int line3 = 2131427429;
 			
 			// aapt resource value: 0x7f0b0009
 			public const int listMode = 2131427337;
@@ -1612,8 +1615,8 @@ namespace Hello
 			// aapt resource value: 0x7f0b0028
 			public const int list_item = 2131427368;
 			
-			// aapt resource value: 0x7f0b005c
-			public const int media_actions = 2131427420;
+			// aapt resource value: 0x7f0b005f
+			public const int media_actions = 2131427423;
 			
 			// aapt resource value: 0x7f0b001a
 			public const int middle = 2131427354;
@@ -1621,11 +1624,11 @@ namespace Hello
 			// aapt resource value: 0x7f0b0013
 			public const int multiply = 2131427347;
 			
-			// aapt resource value: 0x7f0b0057
-			public const int myButton = 2131427415;
+			// aapt resource value: 0x7f0b0059
+			public const int myButton = 2131427417;
 			
-			// aapt resource value: 0x7f0b0066
-			public const int myContacts = 2131427430;
+			// aapt resource value: 0x7f0b0069
+			public const int myContacts = 2131427433;
 			
 			// aapt resource value: 0x7f0b0052
 			public const int name = 2131427410;
@@ -1653,6 +1656,9 @@ namespace Hello
 			
 			// aapt resource value: 0x7f0b003b
 			public const int radio = 2131427387;
+			
+			// aapt resource value: 0x7f0b005b
+			public const int right_drawer = 2131427419;
 			
 			// aapt resource value: 0x7f0b0014
 			public const int screen = 2131427348;
@@ -1699,8 +1705,8 @@ namespace Hello
 			// aapt resource value: 0x7f0b004f
 			public const int select_dialog_listview = 2131427407;
 			
-			// aapt resource value: 0x7f0b0067
-			public const int settings = 2131427431;
+			// aapt resource value: 0x7f0b006a
+			public const int settings = 2131427434;
 			
 			// aapt resource value: 0x7f0b003a
 			public const int shortcut = 2131427386;
@@ -1729,8 +1735,8 @@ namespace Hello
 			// aapt resource value: 0x7f0b0017
 			public const int src_over = 2131427351;
 			
-			// aapt resource value: 0x7f0b005b
-			public const int status_bar_latest_event_content = 2131427419;
+			// aapt resource value: 0x7f0b005e
+			public const int status_bar_latest_event_content = 2131427422;
 			
 			// aapt resource value: 0x7f0b004c
 			public const int submit_area = 2131427404;
@@ -1741,17 +1747,20 @@ namespace Hello
 			// aapt resource value: 0x7f0b000b
 			public const int tabMode = 2131427339;
 			
-			// aapt resource value: 0x7f0b0063
-			public const int text = 2131427427;
+			// aapt resource value: 0x7f0b005a
+			public const int tempButton = 2131427418;
 			
-			// aapt resource value: 0x7f0b0061
-			public const int text2 = 2131427425;
+			// aapt resource value: 0x7f0b0066
+			public const int text = 2131427430;
+			
+			// aapt resource value: 0x7f0b0064
+			public const int text2 = 2131427428;
 			
 			// aapt resource value: 0x7f0b0034
 			public const int textSpacerNoButtons = 2131427380;
 			
-			// aapt resource value: 0x7f0b005f
-			public const int time = 2131427423;
+			// aapt resource value: 0x7f0b0062
+			public const int time = 2131427426;
 			
 			// aapt resource value: 0x7f0b002a
 			public const int title = 2131427370;
@@ -1933,10 +1942,7 @@ namespace Hello
 			public const int select_dialog_singlechoice_material = 2130903078;
 			
 			// aapt resource value: 0x7f030027
-			public const int Settings = 2130903079;
-			
-			// aapt resource value: 0x7f030028
-			public const int support_simple_spinner_dropdown_item = 2130903080;
+			public const int support_simple_spinner_dropdown_item = 2130903079;
 			
 			static Layout()
 			{
